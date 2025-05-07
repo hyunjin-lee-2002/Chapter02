cfb349495364c28ea153dcd6879112eb14a79903
hyunjin-lee-2002 <hyunjinlee2002@gmail.com>
2025-05-07 17:40:22 +0900

    250507(wed)

--- src/com/javaex/ex20/ColorPoint.java
+++ src/com/javaex/ex20/ColorPoint.java
@@ -1,7 +1,82 @@
 package com.javaex.ex20;
 
-public class ColorPoint {
-	
+//public class ColorPoint {
+	public class ColorPoint extends Point {
+
+		// 필드
+		private String color;
+
+		// 생성자
+		public ColorPoint() {
+			System.out.println("ColorPoint()");
+		}
+
+		public ColorPoint(String color) {
+			this.color = color;
+			System.out.println("ColorPoint(1)");
+		}
+
+		public ColorPoint(int x, int y, String color) {
+			//메모리 올리고
+			
+		
+			/*
+			//초기화1 - 부모의 생성자이용
+			super(x, y);
+			this.color = color;
+			*/
+			
+			/*
+			//초기화2 - 부모의 필드에 직접 입력X  setter이용
+			//super();
+			super.setX(x);
+			super.setY(y);
+			this.color = color;
+			*/
+			
+			//초기화3 - 부모의 필드가 protected일때
+			//super();
+			super.x = x;
+			super.y = y;
+			this.color = color;
+			
+			System.out.println("ColorPoint(3)");
+			
+		}
+		
+		
+		// 메소드 gs
+		public String getColor() {
+			return color;
+		}
+
+		public void setColor(String color) {
+			this.color = color;
+		}
+
+		// 메소드 일반
+		@Override
+		public String toString() {
+			return "ColorPoint [color=" + color + ", getX()=" + getX() + ", getY()=" + getY() + "]";
+		}
+		
+		
+		public void draw() {
+			/*
+			System.out.println("========================");
+			System.out.println("x=" + super.getX());
+			System.out.println("y=" + super.getY());
+			System.out.println("color=" + this.color);
+			System.out.println("========================");
+			*/
+			
+			//부모의 필드가 protected 일때
+			System.out.println("========================");
+			System.out.println("x=" + super.x);
+			System.out.println("y=" + super.y);
+			System.out.println("color=" + this.color);
+			System.out.println("========================");
+		}
 	
 	
 }
--- src/com/javaex/ex20/PointApp.java
+++ src/com/javaex/ex20/PointApp.java
@@ -1,10 +1,22 @@
 package com.javaex.ex20;
 
+
 public class PointApp {
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) {	
+
+		Point p = new Point();
+		//System.out.println(p.toString());
 		
 		
+		ColorPoint cp = new ColorPoint(5, 3, "red");
+		System.out.println(cp.toString());
+		
+		cp.draw();
+		
+		//Point(2)
+		//ColorPoint(3)
 		
 	}
 
